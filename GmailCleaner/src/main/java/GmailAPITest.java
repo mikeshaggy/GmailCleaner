b64866e5abc7b2e324a120c06b4dbd7dd8a4b655
mikeshaggy <michalbagan@icloud.com>
2023-12-30 16:57:42 +0100

    Implemented logic of getting message's sender data

--- GmailCleaner/src/main/java/GmailAPITest.java
+++ GmailCleaner/src/main/java/GmailAPITest.java
@@ -10,13 +10,12 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.google.api.services.gmail.model.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 
@@ -39,7 +38,9 @@ public class GmailAPITest {
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_LABELS);
+    private static final List<String> SCOPES = Collections.singletonList("https://www.googleapis.com/auth/gmail.readonly");
+
+
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
     /**
@@ -80,15 +81,101 @@ public class GmailAPITest {
 
         // Print the labels in the user's account.
         String user = "me";
-        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
-        List<Label> labels = listResponse.getLabels();
-        if (labels.isEmpty()) {
-            System.out.println("No labels found.");
+//        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+//        List<Label> labels = listResponse.getLabels();
+//        if (labels.isEmpty()) {
+//            System.out.println("No labels found.");
+//        } else {
+//            System.out.println("Labels:");
+//            for (Label label : labels) {
+//                System.out.printf("- %s\n", label.getName());
+//            }
+//        }
+
+        List<Message> messages = service.users().messages().list(user).execute().getMessages();
+
+        // zapisywanie wiadomości w output.html
+//        Message firstMessage = messages.get(0);
+//        getMessageBody(firstMessage, service, user);
+
+        Message firstMessage = messages.get(2);
+
+        System.out.println(getSender(firstMessage, service, user));
+    }
+
+    private static String getSender(Message rawMessage, Gmail service, String user) throws IOException {
+        Message message = getMessage(rawMessage, service, user);
+
+        String fromHeader = message.getPayload().getHeaders().stream()
+                .filter(header -> "From".equals(header.getName()))
+                .map(MessagePartHeader::getValue)
+                .findFirst()
+                .orElse(null);
+
+        return fromHeader;
+    }
+
+    public static void createHTMLFile(String htmlContent, String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(htmlContent);
+            System.out.println("Plik HTML został utworzony pomyślnie.");
+        } catch (IOException e) {
+            System.err.println("Błąd podczas zapisywania do pliku HTML: " + e.getMessage());
+        }
+    }
+
+    private static Message getMessage(Message rawMessage, Gmail service, String user) throws IOException {
+        String messageId = rawMessage.getId();
+
+        return service.users().messages().get(user, messageId).execute();
+    }
+
+    private static void getMessageBody(Message rawMessage, Gmail service, String user) throws IOException {
+        Message message = getMessage(rawMessage, service, user);
+
+        if (message == null || message.getPayload() == null) {
+            System.out.println("Brak treści wiadomości.");
+            return;
+        }
+
+        MessagePart messagePart = message.getPayload();
+        String mimeType = messagePart.getMimeType();
+        System.out.println(mimeType);
+
+        if ("multipart/alternative".equals(mimeType) && messagePart.getParts() != null) {
+            List<MessagePart> parts = messagePart.getParts();
+            if (!parts.isEmpty()) {
+                for (MessagePart part : parts) {
+                    String partMimeType = part.getMimeType();
+                    if ("text/plain".equals(partMimeType) || "text/html".equals(partMimeType)) {
+                        String bodyData = getBodyData(part);
+                        if (bodyData != null && !bodyData.isEmpty()) {
+                            String bodyText = new String(Base64.getUrlDecoder().decode(bodyData), StandardCharsets.UTF_8);
+                            System.out.println(bodyText);
+                            createHTMLFile(bodyText, "output.html");
+                            return;
+                        }
+                    }
+                }
+            }
         } else {
-            System.out.println("Labels:");
-            for (Label label : labels) {
-                System.out.printf("- %s\n", label.getName());
+            String bodyData = getBodyData(messagePart);
+            if (bodyData != null && !bodyData.isEmpty()) {
+                String bodyText = new String(Base64.getUrlDecoder().decode(bodyData), StandardCharsets.UTF_8);
+                System.out.println(bodyText);
+                createHTMLFile(bodyText, "output.html");
+            } else {
+                System.out.println("Treść wiadomości jest pusta.");
             }
         }
+    }
+
+    private static String getBodyData(MessagePart messagePart) {
+        if (messagePart.getBody() != null) {
+            return messagePart.getBody().getData();
+        } else if (messagePart.getParts() != null && !messagePart.getParts().isEmpty()) {
+            return getBodyData(messagePart.getParts().get(0));
+        }
+        return null;
     }
 }
